e9aac047afdc81c2e31549f0a575568c7de1daf2
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-01-24 05:37:29 +0200

    managed to add dummy images and dynamic colors

--- dom1.js
+++ dom1.js
@@ -1,10 +1,24 @@
-// const myEle = document.querySelector('.myEle');
+let main = document.querySelector("#output");
+
+// "https://dummyimage.com/200x100&text=dummyimage.com+rocks!"
 
 
-eles = document.querySelectorAll(".myEle");
+for (let i = 0; i < 10; i++){
+    main.innerHTML += "<img>";
+}
 
-eles.forEach((ele, e) =>{
-    ele.textContent = `Hello World ${e+1}`;
+const url = "//dummyimage.com/200x100";
+let eles = document.querySelectorAll('img');
+// console.log(eles);
 
-    console.log(ele.textContent);
-})
+eles.forEach((el, index) =>{
+    const myImg = url + `/${randomColor()}/000000&text=New Image+ ${index + 1}`;
+    el.setAttribute('target', '_blank');
+    if(!el.getAttribute('src')){
+        el.setAttribute("src", myImg);
+    }
+});
+
+function randomColor(){
+    return Math.random().toString(16).substr(2,6);
+}
