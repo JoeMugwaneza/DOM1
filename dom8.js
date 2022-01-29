d664b1070bc5e0c05b463ad2471d4a3012e6055f
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-01-29 05:22:06 +0200

    added smooth movement and ability to move within the boundaries of the gameboard

--- dom8.js
+++ dom8.js
@@ -1,40 +1,47 @@
 const output = document.querySelector('.output');
-let game = {x:0, y:0, speed:10, ele:null};
+let game = {x:50, y:50, speed:5, ele:null};
+const keyz = {ArrowRight: false, ArrowLeft:false,ArrowUp:false,ArrowDown:false}
+game.ele = maker();
 
+let move = window.requestAnimationFrame(updatePosition);
 document.addEventListener('keydown', (e) =>{
     console.log(e.key);
-    if(!game.ele){
-        game.ele = maker();
-    }
-
-    if(e.key == 'ArrowRight'){
-        game.x += game.speed;
-        updateProp();
-    }
-
-    if(e.key == 'ArrowLeft'){
-        game.x -= game.speed;
-        updateProp();
-    }
-
-    if(e.key == 'ArrowUp'){
-        game.y -= game.speed;
-        updateProp();
-    }
-    if(e.key == 'ArrowDown'){
-        game.y += game.speed;
-        updateProp();
-    }
+    if(e.code in keyz){keyz[e.code] = true;}
 })
+
 
 
 document.addEventListener('keyup', (e) =>{
-    console.log(e.key);
+    if(e.code in keyz){keyz[e.code] = false;}
 })
 
-function updateProp(){
+function updatePosition(){
+    let domRect = output.getBoundingClientRect();
+    // console.log(domRect);
+    if(keyz.ArrowRight && game.x < (domRect.right - 30)){
+        game.x += game.speed;
+    }
+    
+    if(keyz.ArrowLeft && game.x > domRect.left){
+        game.x -= game.speed;
+    }
+
+    if(keyz.ArrowDown && game.y < (domRect.bottom - 30)){
+        game.y += game.speed;
+    }
+
+    if(keyz.ArrowUp && game.y > domRect.top){
+        game.y -= game.speed;
+    }
+
+
+
+
     game.ele.style.left = game.x + 'px';
     game.ele.style.top = game.y + 'px';
+
+    move = window.requestAnimationFrame(updatePosition)
+
 }
 
 function maker(){
@@ -45,9 +52,9 @@ function maker(){
     el.style.top = game.y + 'px';
     
     el.style.background = '#' +Math.random().toString(16).substr(2,6);
-    el.addEventListener('click', (e) =>{
+
+    el.addEventListener('click', (e)=>{
         game.ele = maker();
-    }, {once:true})
-    
+    },{once:true})
     return el;
 }
